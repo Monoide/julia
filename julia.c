087937f1d098e0d6c8850eaeebb65a73ffa07ecb
Aladin Virmaux <virmaux@lri.com>
2015-12-29 21:10:57 -0500

    licence

--- julia.c
+++ julia.c
@@ -1,14 +1,21 @@
-/* Dessine un ensemble de Julia
- *
- * SDL, les couleurs sont selons les vitesses de divergence
- *
- * Clique droit: zoom centré
- *  -          : unzoom
- *
- * Quelques belles Julia:
- *  0.285 0.01
- *
- *  TODO: *     - rendre propre la partie mouvements SDL, duplication de code
+/*	Julia is a program that draws julia sets.
+    Copyright (C) <2015>  <Aladin Virmaux>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*  TODO: *     - rendre propre la partie mouvements SDL, duplication de code
  *              - choix à l'utisateur pour les couleurs
  *              - ne pas tout recaluler à chaque mouvement
  */
